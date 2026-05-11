d855d73f05f233dedfcc43e2f23bf7a70e1694d0
ITS-NAYAN <nayanmishra0402@gmail.com>
2026-05-11 20:05:28 +0530

    Fix S3 env variables

--- backend/app/s3.py
+++ backend/app/s3.py
@@ -1,21 +1,21 @@
-import boto3
 import os
+import boto3
+from dotenv import load_dotenv
 
-
+load_dotenv()
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = "ap-south-1"  # Mumbai
-BUCKET_NAME = "medsec-upload-bucket"
 
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=AWS_ACCESS_KEY,
-    aws_secret_access_key=AWS_SECRET_KEY,
-    region_name=AWS_REGION
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name="ap-south-1"
 )
 
 
-def upload_file(file, filename):
-    s3.upload_fileobj(file, BUCKET_NAME, filename)
-    return f"https://{BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{filename}"
+def upload_file(file_obj, bucket, key):
+    s3.upload_fileobj(file_obj, bucket, key)
+
+    return f"https://{bucket}.s3.amazonaws.com/{key}"
